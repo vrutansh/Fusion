bcee875ee73dca187cd79dbdb0e8aefea67ecb20
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-12 15:05:00 +0530

    navs

--- src/components/templates/SideNav.jsx
+++ src/components/templates/SideNav.jsx
@@ -11,7 +11,7 @@ const SideNav = () => {
             </h1>
 
             <nav className='flex flex-col text-zinc-400 font-semibold text-2xl gap-3'>
-                <h1 className='text-white font-semibold text-xl mt-10 mb-5'>New Feeds</h1>
+                <h1 className='text-white font-semibold text-xl mt-10 mb-5'>News Feed</h1>
 
                 <Link className='hover:bg-[#6556CD] hover:text-white rounded-xl p-5 duration-300'>
                     <i class="ri-fire-fill mr-2"></i> Trending
--- src/components/templates/TopNav.jsx
+++ src/components/templates/TopNav.jsx
@@ -11,7 +11,7 @@ const TopNav = () => {
         <i class="text-zinc-400 text-3xl ri-close-fill"></i>
 
         <div className='absolute w-[50%] h-[50vh] bg-zinc-200 top-[90%] '>
-            <Link className='hover:text-black hover:bg-zinc-300 font-semibold text-zinc-600 w-[100%] p-10 flex justify-center items-center border-zinc-100 border-b-2'>
+            <Link className='hover:text-black hover:bg-zinc-300 font-semibold text-zinc-600 w-[100%] p-10  flex justify-center items-center border-zinc-100 border-b-2'>
               <img src="" alt="" />
               <span>Hello World</span>
             </Link>
