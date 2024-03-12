a4b92f38eb51c2e0a6a82ddb4fe576f7c28045b5
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-12 13:30:05 +0530

    sidenav done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import SideNav from './templates/SideNav';
+import TopNav from './templates/TopNav';
 
 const Home = () => {
   document.title = "Homepage";
   return (
     <>
       <SideNav/>
-      <div className='w-[80%] h-full '></div>
+      <div className='w-[80%] h-full '>
+        <TopNav/>
+      </div>
     </>
   )
 }
