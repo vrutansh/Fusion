49ff6488e06e9ca90bf6782ae12ec73084891755
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-15 16:21:41 +0530

    Horizontal card

--- src/components/templates/HorizontalCards.jsx
+++ src/components/templates/HorizontalCards.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import DropDown from './DropDown'
+
 
 
 const HorizontalCards = ({data}) => {
   return (
-    <div className='w-full p-5 '>
-          <div className='mb-5 flex justify-between'>
-             <h1 className='text-2xl font-semibold text-zinc-400'>Trending</h1> 
-             <DropDown title="Filter" options={['Tv', 'Movie', 'all']}/>
-          </div>
-
-         
-
-
-        <div className='w-[100%]  flex overflow-y-hidden'>
+        <div className='w-[100%]  flex overflow-y-hidden mb-5 p-5'>
             {data.map((d,i)=> <div key={i} className='min-w-[18%] h-full bg-zinc-900 mr-5 mb-5 '>
 
                 <img className='w-full h-[55%] object-cover' src={`https://image.tmdb.org/t/p/original/${d.backdrop_path || d.poster_path}`} alt="" />
@@ -28,7 +19,7 @@ const HorizontalCards = ({data}) => {
 
             </div>)}
         </div>
-    </div>
+    
   )
 }
 
