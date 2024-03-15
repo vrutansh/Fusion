e8aa21433d49d3e25685245dc90623bc641c630d
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-15 16:20:52 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,22 @@ import TopNav from './templates/TopNav';
 import axios from '../utils/axios'
 import Header from './templates/Header';
 import HorizontalCards from './templates/HorizontalCards';
+import DropDown from './templates/DropDown';
 
 const Home = () => {
   document.title = "Homepage";
  
   const [wallpaper, setwallpaper] = useState(null);
   const [Trending, setTrending] = useState(null);
+  const [category, setcategory] = useState("all");
+
 
   const GetHeaderWallpaper =  async () => {
     try {  
         const {data} = await axios.get(`/trending/all/day`);
-        
-        setwallpaper(data.results);
-      let datarandom = data.results[(Math.random()*data.results.length).toFixed()];
-      setwallpaper(datarandom)
+       
+       let datarandom = data.results[(Math.random()*data.results.length).toFixed()];
+       setwallpaper(datarandom)
         
     } catch(e){
         console.log("error", e);
@@ -26,12 +28,8 @@ const Home = () => {
  
   const GetTrending =  async () => {
     try {  
-        const {data} = await axios.get(`/trending/all/day`);
-        
-        
-      
-      setTrending(data.results)
-        
+        const {data} = await axios.get(`/trending/${category}/day`);
+         setTrending(data.results);
     } catch(e){
         console.log("error", e);
     }
@@ -40,17 +38,24 @@ const Home = () => {
 
 
   useEffect(() =>{
+    GetTrending();
     !wallpaper && GetHeaderWallpaper();
-    !Trending  && GetTrending();
-  },[])
+  },[category]);
 
 
   return wallpaper && Trending ? (
     <>
       <SideNav/>
+      
       <div className='w-[80%] h-full overflow-auto overflow-x-hidden'>
         <TopNav/>
         <Header data={wallpaper}/>
+
+        <div className='p-6 flex justify-between'>
+             <h1 className='text-2xl font-semibold text-zinc-400'>Trending</h1> 
+             <DropDown title="Filter" options={['Tv', 'Movie', 'all']} func={(e)=>setcategory(e.target.value)}/>
+          </div>
+
         <HorizontalCards data={Trending}/>
       </div>
     </>
