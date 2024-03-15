b779ef3ba05ab99260735e393ebed610c3b6139b
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-15 16:21:09 +0530

    dropdown changed

--- src/components/templates/DropDown.jsx
+++ src/components/templates/DropDown.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
-const DropDown = ({title, options}) => {
+const DropDown = ({title, options, func}) => {
   return (
     <div className='select'>
-        <select defaultValue="0" name="format" id="format">
+        <select defaultValue="0" onChange={func} name="format" id="format">
            <option value="0" disabled>
                 {title}
-           </option>
-           {options.map((o,i)=>(
-              <option value={o} disabled>
+           </option>  
+        
+          {options.map((o,i)=>(
+              <option key={i} value={o}>
                   {o.toUpperCase()}
              </option>
            ))}
         </select>
    </div>
-  )
-}
+  );
+};
 
 export default DropDown;
