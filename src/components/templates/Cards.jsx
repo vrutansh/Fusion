bc5b3a45b2b8b82be1c4c5512015754014cc8e89
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-16 21:21:06 +0530

    made cards component

---
+++ src/components/templates/Cards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Cards = ({data, title}) => {
+  return (
+    <div className='flex flex-wrap w-full'>
+        {data.map((c,i)=>( <Link className='w-[29vh] py-4  mr-[5%] mb-[5%]' key={i}>
+
+            <img className='shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] object-cover' src={`https://image.tmdb.org/t/p/original/${c.poster_path || c.backdrop_path}`} alt="" />
+
+            <h1 className='text-2xl text-zinc-400 mt-3 font-semibold'>{ c.name || c.title || c.original_title || c.original_name}</h1>
+        </Link>))}
+    </div>
+  )
+}
+
+export default Cards
