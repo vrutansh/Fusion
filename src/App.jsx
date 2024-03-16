3dc8f287c0916c80df62d4326f8e4d6c26f154a5
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-16 12:30:33 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
+import Trending from './components/Trending'
 
 function App() {
  
@@ -12,7 +13,7 @@ function App() {
      <div className='bg-[#1F1E24] w-screen h-full flex'>
       <Routes>
         <Route path='/' element={<Home />} />
-      
+        <Route path='/trending' element={<Trending/>} />
       </Routes>
      </div>
     </>
