a89f1dc692db2b4aa324ee36a0756be4e6626eef
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-16 21:22:07 +0530

    Made the trending route and structured it

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import TopNav from './templates/TopNav';
 import DropDown from './templates/DropDown';
+import axios from '../utils/axios';
+import Cards from './templates/Cards';
+
 
 const Trending = () => {
    const navigate = useNavigate();
-  return (
-    <div className='p-[3%] w-screen h-screen '>
+   const [category, setcategory] = useState('all');
+   const [duration, setduration] = useState('day');
+   const [trending, settrending] = useState([]);
+
+
+   const GetTrending =  async () => {
+    try {  
+         const { data } = await axios.get(`/trending/${category}/${duration}`);
+         settrending(data.results);
+         
+    } catch(error){
+        console.log("error", error);
+    }
+  }; 
+
+  useEffect(() =>{
+    GetTrending();
+  },[category, duration]);
+
+  return trending.length>0 ? (
+    <div className='px-[3%] w-screen h-screen overflow-hidden overflow-y-auto '>
         <div className='w-full flex items-center justify-between '>
           
             <h1 className=' text-3xl text-zinc-400 font-semibold'>
-                <i onClick={()=> navigate(-1)} class='hover:text-[#6556CD] ri-arrow-left-line'></i> Trending
+                <i onClick={()=> navigate(-1)} class='hover:text-[#6556CD] ri-arrow-left-line'/> Trending
             </h1>
 
            <div className='flex items-center w-[80%]'>
                <TopNav/>
-               <DropDown title="Category" options={["Movie", "TV", "All"]} func=""/>
+               <DropDown title="Category" options={["Movie", "TV", "All"]} func={
+                (e)=>setcategory(e.target.value)}/>
+
                 <div className='w-[2%]'></div>
-               <DropDown title="Duration" options={["Week", "Day"]} func=""/>
+               <DropDown title="Duration" options={["Week", "Day"]} func={
+                (e)=>setduration(e.target.value)}/>
            </div>
         </div>
+
+        <Cards data={trending} title={category}/>
     </div>
-  )
+  ):<h1>Loading</h1>
 }
 
 export default Trending
