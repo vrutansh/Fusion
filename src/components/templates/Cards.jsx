ca6da5f49fb95aa4694d2226b3371c4645324997
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-17 15:08:49 +0530

    changed the styling of CArds

--- src/components/templates/Cards.jsx
+++ src/components/templates/Cards.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Cards = ({data, title}) => {
   return (
-    <div className='flex flex-wrap w-full'>
+    <div className='flex flex-wrap w-full h-full px-[3%] bg-[#1F1E24]'>
         {data.map((c,i)=>( <Link className='w-[29vh] py-4  mr-[5%] mb-[5%]' key={i}>
 
             <img className='shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] object-cover' src={`https://image.tmdb.org/t/p/original/${c.poster_path || c.backdrop_path}`} alt="" />
