79c77dd1bb39b344534f401d31cd2fcbb5398b98
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-17 15:10:03 +0530

    Trending page done with Infinite scrollers and filters working..

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -4,6 +4,7 @@ import TopNav from './templates/TopNav';
 import DropDown from './templates/DropDown';
 import axios from '../utils/axios';
 import Cards from './templates/Cards';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 const Trending = () => {
@@ -11,25 +12,43 @@ const Trending = () => {
    const [category, setcategory] = useState('all');
    const [duration, setduration] = useState('day');
    const [trending, settrending] = useState([]);
-
+   const [page, setpage] = useState([1]);
+   const [hasMore, sethasMore] = useState(true)
 
    const GetTrending =  async () => {
     try {  
-         const { data } = await axios.get(`/trending/${category}/${duration}`);
-         settrending(data.results);
+         const { data } = await axios.get(`/trending/${category}/${duration}?page=${page}?language=en-US`);
+        //  settrending(data.results);
+        if(data.results.length > 0){
+          settrending((prevState)=>[...prevState, ...data.results])
+          setpage(page+1)
+        }else{
+          sethasMore(false)
+        }
          
     } catch(error){
-        console.log("error", error);
+        console.log("error:", error);
     }
   }; 
 
+  const refreshHandler = () =>{
+      if(trending.length === 0){
+         GetTrending()
+      }
+      else{
+         setpage(1)
+         settrending([])
+         GetTrending()
+      }
+  }
+
   useEffect(() =>{
-    GetTrending();
+    refreshHandler();
   },[category, duration]);
 
   return trending.length>0 ? (
-    <div className='px-[3%] w-screen h-screen overflow-hidden overflow-y-auto '>
-        <div className='w-full flex items-center justify-between '>
+    <div className='w-screen h-screen  '>
+        <div className='px-[2%] w-full flex items-center justify-between '>
           
             <h1 className=' text-3xl text-zinc-400 font-semibold'>
                 <i onClick={()=> navigate(-1)} class='hover:text-[#6556CD] ri-arrow-left-line'/> Trending
@@ -37,16 +56,18 @@ const Trending = () => {
 
            <div className='flex items-center w-[80%]'>
                <TopNav/>
-               <DropDown title="Category" options={["Movie", "TV", "All"]} func={
+               <DropDown title="Category" options={["movie", "tv", "All"]} func={
                 (e)=>setcategory(e.target.value)}/>
 
                 <div className='w-[2%]'></div>
-               <DropDown title="Duration" options={["Week", "Day"]} func={
+               <DropDown title="Duration" options={["week", "day"]} func={
                 (e)=>setduration(e.target.value)}/>
            </div>
         </div>
-
-        <Cards data={trending} title={category}/>
+        <InfiniteScroll dataLength={trending.length} next={GetTrending} hasMore={hasMore} loader={<h1>Loading....</h1>}>
+         <Cards data={trending} title={category}/>
+        </InfiniteScroll>
+        
     </div>
   ):<h1>Loading</h1>
 }
